fca06b5155c327d988091ed990746632182e7b78
Diego R. Ammirabile <diegoroberto.ammmirabile@gmail.com>
2022-02-01 14:58:53 +0100

    Non avevo visto la funzione shuffleM

--- mescolatore.hs
+++ mescolatore.hs
@@ -1,20 +1,10 @@
 import System.IO
-import System.Random
+import System.Random.Shuffle
 
 getFileLines path = lines <$> readFile path
 
 randomizeList :: [a] -> IO [a]
-randomizeList [] = return []
-randomizeList l = do
-    randNum <- randomRIO (0::Int, length l -1)
-    tail <- randomizeList $ rm l randNum
-    return (l !! randNum : tail)
-
-rm :: [a] -> Int -> [a]
-rm [] _ = []
-rm (n:l) i 
-  | i >= 0 = if i/=0 then n:rm l (i-1) else l
-  | otherwise = error ("per qualche motivo sono arrivato a i=" ++ show i)
+randomizeList = shuffleM
 
 main = hSetBuffering stdin LineBuffering >> getFileLines "Nomi.txt" >>= randomizeList >>= printLines >> getChar 
 
